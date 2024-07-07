27e6ea9c5f2e6f4aec6759ab5aeb73bb0a3b796f
elychirivella10 <50273692+elychirivella10@users.noreply.github.com>
2024-07-07 18:16:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
+const cors = require('cors');
+app.use(cors());
 app.listen(app.get('port'),()=>{
 console.log("servidor esuchando en el puerto", app.get("port"));
 });
